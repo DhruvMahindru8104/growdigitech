33bb24ddda671cf9c2c038341f105c3f8c169d5b
DhruvMahindru8104 <dhruv.mahindru08@gmail.com>
2025-07-18 14:51:09 +0530

    second commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,13 +6,13 @@ import TeamSection from "@/components/mycomponents/team";
 export default function Home() {
   return (
    <>
-   <div className="max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 space-y-16 ">
+   
 <Navbar/>
    <HeroSection/>
    <ContentSection/>
    <GrowUtilitiesSection/>
    <TeamSection/>
-   </div>
+   
    
    </>
   );
