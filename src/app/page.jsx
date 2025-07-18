2f4822fcf14bd6e7504cd5889770f43371d920cb
DhruvMahindru8104 <dhruv.mahindru08@gmail.com>
2025-07-18 16:46:57 +0530

    removing space

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
    <ContentSection/>
    <GrowUtilitiesSection/>
    <TeamSection/>
-    <div className="max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 space-y-16 ">
+    <div className="max-w-7xl mx-auto px-5 sm:px-10 md:px-12 lg:px-5 space-y-16 overflow-hidden ">
    <AnimatedTestimonialsDemo/>
     </div>
 
